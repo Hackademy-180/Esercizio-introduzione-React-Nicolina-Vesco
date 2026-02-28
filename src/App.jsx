fb1c3829329e5730c78500797d657f896c5c0347
Nicolina <nicolina.vesco@gmail.com>
2026-02-28 10:47:35 +0100

    Esercizio introduzione react

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+function App() {
+
+  return (
+    <>
+      <h1 className="title">Introduzione React</h1>
+      <input type="text" className='input-text' placeholder='Scrivi il tuo testo qui'/>
+      <lavel htmlFor="text" className='label-text' />
+    </>
+  )
+}
+
+export default App
